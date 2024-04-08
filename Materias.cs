92ecfb5d6c7be37f5ea8c87a35ac4f91adf8e258
lameca <Aylincontreras06poomaray@gmail.com>
2024-04-08 11:24:56 -0400

    Creacion de formulario de materia

---
+++ Materias.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestor_calificaciones
+{
+    public partial class Materias : Form
+    {
+        public Materias()
+        {
+            InitializeComponent();
+        }
+
+        private void Materias_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
