a8daf48d784da662a1d74157ee1dc0d1d4647b6a
aminsito1104 <164044787+aminsito1104@users.noreply.github.com>
2024-05-07 23:03:20 -0400

    Create InsertEstudiante.sql
    para insertar estudiantes

---
+++ Migrations/InsertEstudiante.sql
@@ -0,0 +1,8 @@
+EXEC INSERT_ESTUDIANTE 
+    @primer_nombre = 'Ana',
+    @segundo_nombre = 'María',
+    @primer_apellido = 'García',
+    @segundo_apellido = 'Pérez',
+    @grado = 9,
+    @num_lista = 18,
+    @fecha_nacimiento = '2003-05-20';
