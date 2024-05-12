7450afaca178cdd0b1e0d7eb29f76e7c2c317933
Geraldo204 <ysandis10germel@gmail.com>
2024-05-11 21:45:04 -0400

    Boletin calificaciones

---
+++ Forms/Boletin de notas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestor_calificaciones.Forms
+{
+    public partial class Boletin_de_notas : Form
+    {
+        public Boletin_de_notas()
+        {
+            InitializeComponent();
+        }
+    }
+}
